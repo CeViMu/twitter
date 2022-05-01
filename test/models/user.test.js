ac97690c74345bea2d72f3ad466eee1abaf75a77
CeViMu <vce02060@innovaccion.mx>
2022-05-01 18:54:21 -0500

    Prueba del segundo requerimiento con error

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,10 +1,15 @@
 const User = require('./../../app/models/user')
 
 describe("Unit Test for User class", () => {
-    test ("Create an User object", () => {
+    test ("Requerimiento 1: Creación de User", () => {
         const user = new User (1,"CeciliaMuC","Cecilia","Bio")
         expect(user.id).toBe(1)
         expect(user.username).toBe("CeciliaMuC")
     })
   
+    test("Requerimiento 2: Fechas en atributos de User", ()=>{
+        const user = new User(1,"CeciliaMuC","Cecilia","Bio")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdate).not.toBeUndefined()
+    })
 })
