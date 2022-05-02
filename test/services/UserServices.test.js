286cd307932031b4d17f964f8c225e449573a471
CeViMu <vce02060@innovaccion.mx>
2022-05-01 20:14:42 -0500

    Agregando requerimiento 2 de Services

--- test/services/UserServices.test.js
+++ test/services/UserServices.test.js
@@ -8,4 +8,14 @@ describe("Test para UserService", ()=>{
         expect(user.name).toBe("Cecilia")
         expect(user.bio).not.toBeUndefined()
     })
+
+    test("Requerimiento 2: Obtener todos datos de usuario en una lista", ()=>{
+        const user =UserService.create(1,"CeciliaMuC","Cecilia")
+        const userInfoInList = UserService.getInfo(user)
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("CeciliaMuC")
+        expect(userInfoInList[2]).toBe("Cecilia")
+        expect(userInfoInList[3]).toBe("Sin bio")
+    })
+
 })
